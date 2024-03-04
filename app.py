41fd56006492cb14ee0d569a3040db14efd92bed
陳冠邑 <100064250+chenkuanyi0531@users.noreply.github.com>
2024-03-04 14:00:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
 openai.api_key = os.getenv("AZURE_OPENAI_API_KEY")
 openai.api_base = os.getenv("AZURE_OPENAI_ENDPOINT")
 openai.api_type = 'azure'
-
+openai.api_version = '2024-02-15-preview'
 def GPT_response(text):
     # 接收回應
     response = openai.Completion.create(engine="gpt-4-turbo", prompt=text, temperature=0.7, max_tokens=800,top_p=0.95)
