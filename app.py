835d08283ca7b185ed1dc4ac2e4c0e6a57f040a8
陳冠邑 <100064250+chenkuanyi0531@users.noreply.github.com>
2024-03-04 22:37:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def GPT_response(text):
     # 接收回應
     print("處理中請稍後")
     messages_text=[
-         {"role": "system", "content": "你是一位智能小幫手，幫助人們查找資料"},
+         {"role": "system", "content": "你是一位智能小幫手，幫助人們查找資料，所有回復請說繁體中文"},
          {"role": "user", "content": text},
     ]
     response = openai.ChatCompletion.create(engine="gpt-4-turbo1", messages=messages_text, temperature=0.9, max_tokens=1000,top_p=0.95)
